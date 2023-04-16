c878287ebeef6e9f7c3551a53847a43f072c9f1b
KAIKI DA SILVA COSTA <130805988+Kaykysv@users.noreply.github.com>
2023-04-16 13:15:26 -0300

    Add files via upload

---
+++ public class Teste_Metodo_Static {.java
@@ -0,0 +1,10 @@
+public class Teste_Metodo_Static {
+
+	public static void main(String[] args) {
+		double num1 = 8.5;
+		double pi = Math.PI;
+
+		System.out.println("Valor num1 = "+num1);
+		System.out.println("Valor PI = "+pi);
+
+		System.out.println("Soma dos valores = "+(num1+pi));
